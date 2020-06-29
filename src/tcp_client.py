e2d9bec373e8da80bf8ba2afb9b0ecefc6d8c161
BitFreak <blw_matt@rocketmail.com>
2020-06-29 19:51:29 +0300

    schimbari minore la tcp_client

--- src/tcp_client.py
+++ src/tcp_client.py
@@ -8,19 +8,29 @@ logging.basicConfig(format = u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s] 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
 
-port = 10000
-adresa = 'localhost'
+port = 10040
+adresa = '198.7.0.2'
 server_address = (adresa, port)
-mesaj = sys.argv[1]
+
+# pentru acest experiment clientul se va conecta la un singur server
+logging.info('Handshake cu %s', str(server_address))
+sock.connect(server_address)
+cnt_mesaj = 1
 
 try:
-    logging.info('Handshake cu %s', str(server_address))
-    sock.connect(server_address)
-    time.sleep(3)
-    sock.send(mesaj.encode('utf-8'))
-    data = sock.recv(1024)
-    logging.info('Content primit: "%s"', data)
+    while True:
+        time.sleep(5)
+        mesaj = 'mesajul_' + str(cnt_mesaj)
+        sock.send(mesaj.encode('utf-8'))
+        logging.info('Mesaj trimis: "%s"', mesaj)
+        data = sock.recv(1024)
+        logging.info('Content primit: "%s"', data)
+        cnt_mesaj += 1
+
+except KeyboardInterrupt:
+    print("except")
 
 finally:
     logging.info('closing socket')
     sock.close()
+    exit(0)
