927a490679149bec2f12987fe0834b8c90b2c8ed
BitFreak <blw_matt@rocketmail.com>
2020-06-29 19:48:33 +0300

    schimbari minore la tcp_server si tcp_client

--- src/tcp_server.py
+++ src/tcp_server.py
@@ -7,19 +7,31 @@ logging.basicConfig(format = u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s] 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
 
-port = 10000
-adresa = 'localhost'
+port = 10040
+adresa = '0.0.0.0'
 server_address = (adresa, port)
 sock.bind(server_address)
-logging.info("Serverul a pornit pe %s si portnul portul %d", adresa, port)
+logging.info("Serverul a pornit pe %s si portul %d", adresa, port)
 sock.listen(5)
-while True:
-    logging.info('Asteptam conexiui...')
-    conexiune, address = sock.accept()
-    logging.info("Handshake cu %s", address)
-    time.sleep(2)
-    data = conexiune.recv(1024)
-    logging.info('Content primit: "%s"', data)
-    conexiune.send(b"Server a primit mesajul: " + data)
+
+# pentru acest experiment la server se va conecta un singur client
+logging.info('Asteptam conexiuni...')
+conexiune, address = sock.accept()
+logging.info("Handshake cu %s", address)
+
+
+try:
+    while True:
+        time.sleep(5)
+        data = conexiune.recv(1024)
+
+        logging.info('Content primit: "%s"', data)
+        conexiune.send(b"Server a primit mesajul: " + data)
+        logging.info(f"Content trimis: Server a primit mesajul: {data}")
+
+except KeyboardInterrupt:
+    print("except")
+finally:
     conexiune.close()
-sock.close()
+    sock.close()
+    exit(0)
